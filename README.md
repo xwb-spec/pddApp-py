a313e1c5e42df649e88967d6117f09b0a892d28e
Gogs <gogs@fake.local>
2022-08-30 23:39:00 +0800

    test

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # pddApp
+pip3 install pyside6
+ bin/pyside6-designer /Users/xiewenbin/PycharmProjects/pddApp/untitled.ui -o  /Users/xiewenbin/PycharmProjects/pddAp/ui_mainwindow.py
+
+
+Error: dyld: could not load inserted library '/Applications/Xcode.app/Contents/Developer/Library/Frameworks/Python3.framework/Versions/3.8/Python3' because image not found
---
+++ ui_untitled.py
@@ -0,0 +1,381 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'untitled.ui'
+##
+## Created by: Qt User Interface Compiler version 6.3.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QCheckBox, QComboBox, QHBoxLayout,
+    QLabel, QLineEdit, QMainWindow, QPushButton,
+    QSizePolicy, QStatusBar, QTextBrowser, QToolButton,
+    QWidget)
+
+class Ui_pddApp(object):
+    def setupUi(self, pddApp):
+        if not pddApp.objectName():
+            pddApp.setObjectName(u"pddApp")
+        pddApp.resize(808, 668)
+        pddApp.setMaximumSize(QSize(1239, 16777215))
+        self.centralwidget = QWidget(pddApp)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.layoutWidget = QWidget(self.centralwidget)
+        self.layoutWidget.setObjectName(u"layoutWidget")
+        self.layoutWidget.setGeometry(QRect(20, 100, 771, 31))
+        self.horizontalLayout_4 = QHBoxLayout(self.layoutWidget)
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+        self.horizontalLayout_4.setContentsMargins(0, 0, 0, 0)
+        self.label_5 = QLabel(self.layoutWidget)
+        self.label_5.setObjectName(u"label_5")
+
+        self.horizontalLayout_4.addWidget(self.label_5)
+
+        self.lineEdit_4 = QLineEdit(self.layoutWidget)
+        self.lineEdit_4.setObjectName(u"lineEdit_4")
+
+        self.horizontalLayout_4.addWidget(self.lineEdit_4)
+
+        self.toolButton_2 = QToolButton(self.layoutWidget)
+        self.toolButton_2.setObjectName(u"toolButton_2")
+
+        self.horizontalLayout_4.addWidget(self.toolButton_2)
+
+        self.layoutWidget_2 = QWidget(self.centralwidget)
+        self.layoutWidget_2.setObjectName(u"layoutWidget_2")
+        self.layoutWidget_2.setGeometry(QRect(20, 130, 771, 31))
+        self.horizontalLayout_5 = QHBoxLayout(self.layoutWidget_2)
+        self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
+        self.horizontalLayout_5.setContentsMargins(0, 0, 0, 0)
+        self.label_6 = QLabel(self.layoutWidget_2)
+        self.label_6.setObjectName(u"label_6")
+
+        self.horizontalLayout_5.addWidget(self.label_6)
+
+        self.lineEdit_5 = QLineEdit(self.layoutWidget_2)
+        self.lineEdit_5.setObjectName(u"lineEdit_5")
+
+        self.horizontalLayout_5.addWidget(self.lineEdit_5)
+
+        self.toolButton_3 = QToolButton(self.layoutWidget_2)
+        self.toolButton_3.setObjectName(u"toolButton_3")
+
+        self.horizontalLayout_5.addWidget(self.toolButton_3)
+
+        self.layoutWidget_3 = QWidget(self.centralwidget)
+        self.layoutWidget_3.setObjectName(u"layoutWidget_3")
+        self.layoutWidget_3.setGeometry(QRect(20, 160, 771, 31))
+        self.horizontalLayout_6 = QHBoxLayout(self.layoutWidget_3)
+        self.horizontalLayout_6.setObjectName(u"horizontalLayout_6")
+        self.horizontalLayout_6.setContentsMargins(0, 0, 0, 0)
+        self.label_7 = QLabel(self.layoutWidget_3)
+        self.label_7.setObjectName(u"label_7")
+
+        self.horizontalLayout_6.addWidget(self.label_7)
+
+        self.lineEdit_6 = QLineEdit(self.layoutWidget_3)
+        self.lineEdit_6.setObjectName(u"lineEdit_6")
+
+        self.horizontalLayout_6.addWidget(self.lineEdit_6)
+
+        self.toolButton_4 = QToolButton(self.layoutWidget_3)
+        self.toolButton_4.setObjectName(u"toolButton_4")
+
+        self.horizontalLayout_6.addWidget(self.toolButton_4)
+
+        self.layoutWidget_4 = QWidget(self.centralwidget)
+        self.layoutWidget_4.setObjectName(u"layoutWidget_4")
+        self.layoutWidget_4.setGeometry(QRect(20, 210, 771, 31))
+        self.horizontalLayout_8 = QHBoxLayout(self.layoutWidget_4)
+        self.horizontalLayout_8.setObjectName(u"horizontalLayout_8")
+        self.horizontalLayout_8.setContentsMargins(0, 0, 0, 0)
+        self.label_9 = QLabel(self.layoutWidget_4)
+        self.label_9.setObjectName(u"label_9")
+
+        self.horizontalLayout_8.addWidget(self.label_9)
+
+        self.lineEdit_8 = QLineEdit(self.layoutWidget_4)
+        self.lineEdit_8.setObjectName(u"lineEdit_8")
+
+        self.horizontalLayout_8.addWidget(self.lineEdit_8)
+
+        self.toolButton_5 = QToolButton(self.layoutWidget_4)
+        self.toolButton_5.setObjectName(u"toolButton_5")
+
+        self.horizontalLayout_8.addWidget(self.toolButton_5)
+
+        self.layoutWidget_5 = QWidget(self.centralwidget)
+        self.layoutWidget_5.setObjectName(u"layoutWidget_5")
+        self.layoutWidget_5.setGeometry(QRect(20, 240, 245, 23))
+        self.horizontalLayout_9 = QHBoxLayout(self.layoutWidget_5)
+        self.horizontalLayout_9.setObjectName(u"horizontalLayout_9")
+        self.horizontalLayout_9.setContentsMargins(0, 0, 0, 0)
+        self.label_10 = QLabel(self.layoutWidget_5)
+        self.label_10.setObjectName(u"label_10")
+
+        self.horizontalLayout_9.addWidget(self.label_10)
+
+        self.lineEdit_9 = QLineEdit(self.layoutWidget_5)
+        self.lineEdit_9.setObjectName(u"lineEdit_9")
+
+        self.horizontalLayout_9.addWidget(self.lineEdit_9)
+
+        self.layoutWidget_6 = QWidget(self.centralwidget)
+        self.layoutWidget_6.setObjectName(u"layoutWidget_6")
+        self.layoutWidget_6.setGeometry(QRect(340, 240, 245, 23))
+        self.horizontalLayout_10 = QHBoxLayout(self.layoutWidget_6)
+        self.horizontalLayout_10.setObjectName(u"horizontalLayout_10")
+        self.horizontalLayout_10.setContentsMargins(0, 0, 0, 0)
+        self.label_11 = QLabel(self.layoutWidget_6)
+        self.label_11.setObjectName(u"label_11")
+
+        self.horizontalLayout_10.addWidget(self.label_11)
+
+        self.lineEdit_10 = QLineEdit(self.layoutWidget_6)
+        self.lineEdit_10.setObjectName(u"lineEdit_10")
+
+        self.horizontalLayout_10.addWidget(self.lineEdit_10)
+
+        self.layoutWidget_7 = QWidget(self.centralwidget)
+        self.layoutWidget_7.setObjectName(u"layoutWidget_7")
+        self.layoutWidget_7.setGeometry(QRect(20, 260, 771, 31))
+        self.horizontalLayout_11 = QHBoxLayout(self.layoutWidget_7)
+        self.horizontalLayout_11.setObjectName(u"horizontalLayout_11")
+        self.horizontalLayout_11.setContentsMargins(0, 0, 0, 0)
+        self.label_12 = QLabel(self.layoutWidget_7)
+        self.label_12.setObjectName(u"label_12")
+
+        self.horizontalLayout_11.addWidget(self.label_12)
+
+        self.lineEdit_11 = QLineEdit(self.layoutWidget_7)
+        self.lineEdit_11.setObjectName(u"lineEdit_11")
+
+        self.horizontalLayout_11.addWidget(self.lineEdit_11)
+
+        self.toolButton_6 = QToolButton(self.layoutWidget_7)
+        self.toolButton_6.setObjectName(u"toolButton_6")
+
+        self.horizontalLayout_11.addWidget(self.toolButton_6)
+
+        self.layoutWidget_8 = QWidget(self.centralwidget)
+        self.layoutWidget_8.setObjectName(u"layoutWidget_8")
+        self.layoutWidget_8.setGeometry(QRect(20, 290, 245, 23))
+        self.horizontalLayout_12 = QHBoxLayout(self.layoutWidget_8)
+        self.horizontalLayout_12.setObjectName(u"horizontalLayout_12")
+        self.horizontalLayout_12.setContentsMargins(0, 0, 0, 0)
+        self.label_13 = QLabel(self.layoutWidget_8)
+        self.label_13.setObjectName(u"label_13")
+
+        self.horizontalLayout_12.addWidget(self.label_13)
+
+        self.lineEdit_12 = QLineEdit(self.layoutWidget_8)
+        self.lineEdit_12.setObjectName(u"lineEdit_12")
+
+        self.horizontalLayout_12.addWidget(self.lineEdit_12)
+
+        self.checkBox_3 = QCheckBox(self.centralwidget)
+        self.checkBox_3.setObjectName(u"checkBox_3")
+        self.checkBox_3.setGeometry(QRect(230, 350, 77, 22))
+        self.textBrowser = QTextBrowser(self.centralwidget)
+        self.textBrowser.setObjectName(u"textBrowser")
+        self.textBrowser.setGeometry(QRect(20, 380, 651, 261))
+        self.pushButton_3 = QPushButton(self.centralwidget)
+        self.pushButton_3.setObjectName(u"pushButton_3")
+        self.pushButton_3.setGeometry(QRect(680, 310, 113, 41))
+        self.pushButton_4 = QPushButton(self.centralwidget)
+        self.pushButton_4.setObjectName(u"pushButton_4")
+        self.pushButton_4.setGeometry(QRect(680, 360, 113, 41))
+        self.pushButton_5 = QPushButton(self.centralwidget)
+        self.pushButton_5.setObjectName(u"pushButton_5")
+        self.pushButton_5.setGeometry(QRect(680, 410, 113, 41))
+        self.layoutWidget1 = QWidget(self.centralwidget)
+        self.layoutWidget1.setObjectName(u"layoutWidget1")
+        self.layoutWidget1.setGeometry(QRect(20, 10, 771, 33))
+        self.horizontalLayout = QHBoxLayout(self.layoutWidget1)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.label = QLabel(self.layoutWidget1)
+        self.label.setObjectName(u"label")
+
+        self.horizontalLayout.addWidget(self.label)
+
+        self.lineEdit = QLineEdit(self.layoutWidget1)
+        self.lineEdit.setObjectName(u"lineEdit")
+
+        self.horizontalLayout.addWidget(self.lineEdit)
+
+        self.label_2 = QLabel(self.layoutWidget1)
+        self.label_2.setObjectName(u"label_2")
+
+        self.horizontalLayout.addWidget(self.label_2)
+
+        self.lineEdit_2 = QLineEdit(self.layoutWidget1)
+        self.lineEdit_2.setObjectName(u"lineEdit_2")
+
+        self.horizontalLayout.addWidget(self.lineEdit_2)
+
+        self.pushButton = QPushButton(self.layoutWidget1)
+        self.pushButton.setObjectName(u"pushButton")
+
+        self.horizontalLayout.addWidget(self.pushButton)
+
+        self.layoutWidget2 = QWidget(self.centralwidget)
+        self.layoutWidget2.setObjectName(u"layoutWidget2")
+        self.layoutWidget2.setGeometry(QRect(21, 70, 771, 31))
+        self.horizontalLayout_3 = QHBoxLayout(self.layoutWidget2)
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.horizontalLayout_3.setContentsMargins(0, 0, 0, 0)
+        self.label_4 = QLabel(self.layoutWidget2)
+        self.label_4.setObjectName(u"label_4")
+
+        self.horizontalLayout_3.addWidget(self.label_4)
+
+        self.lineEdit_3 = QLineEdit(self.layoutWidget2)
+        self.lineEdit_3.setObjectName(u"lineEdit_3")
+
+        self.horizontalLayout_3.addWidget(self.lineEdit_3)
+
+        self.toolButton = QToolButton(self.layoutWidget2)
+        self.toolButton.setObjectName(u"toolButton")
+
+        self.horizontalLayout_3.addWidget(self.toolButton)
+
+        self.layoutWidget3 = QWidget(self.centralwidget)
+        self.layoutWidget3.setObjectName(u"layoutWidget3")
+        self.layoutWidget3.setGeometry(QRect(20, 190, 245, 23))
+        self.horizontalLayout_7 = QHBoxLayout(self.layoutWidget3)
+        self.horizontalLayout_7.setObjectName(u"horizontalLayout_7")
+        self.horizontalLayout_7.setContentsMargins(0, 0, 0, 0)
+        self.label_8 = QLabel(self.layoutWidget3)
+        self.label_8.setObjectName(u"label_8")
+
+        self.horizontalLayout_7.addWidget(self.label_8)
+
+        self.lineEdit_7 = QLineEdit(self.layoutWidget3)
+        self.lineEdit_7.setObjectName(u"lineEdit_7")
+
+        self.horizontalLayout_7.addWidget(self.lineEdit_7)
+
+        self.layoutWidget4 = QWidget(self.centralwidget)
+        self.layoutWidget4.setObjectName(u"layoutWidget4")
+        self.layoutWidget4.setGeometry(QRect(20, 320, 386, 26))
+        self.horizontalLayout_13 = QHBoxLayout(self.layoutWidget4)
+        self.horizontalLayout_13.setObjectName(u"horizontalLayout_13")
+        self.horizontalLayout_13.setContentsMargins(0, 0, 0, 0)
+        self.label_14 = QLabel(self.layoutWidget4)
+        self.label_14.setObjectName(u"label_14")
+
+        self.horizontalLayout_13.addWidget(self.label_14)
+
+        self.comboBox_2 = QComboBox(self.layoutWidget4)
+        self.comboBox_2.addItem("")
+        self.comboBox_2.setObjectName(u"comboBox_2")
+
+        self.horizontalLayout_13.addWidget(self.comboBox_2)
+
+        self.label_15 = QLabel(self.layoutWidget4)
+        self.label_15.setObjectName(u"label_15")
+
+        self.horizontalLayout_13.addWidget(self.label_15)
+
+        self.comboBox_3 = QComboBox(self.layoutWidget4)
+        self.comboBox_3.addItem("")
+        self.comboBox_3.setObjectName(u"comboBox_3")
+
+        self.horizontalLayout_13.addWidget(self.comboBox_3)
+
+        self.layoutWidget5 = QWidget(self.centralwidget)
+        self.layoutWidget5.setObjectName(u"layoutWidget5")
+        self.layoutWidget5.setGeometry(QRect(20, 350, 281, 22))
+        self.horizontalLayout_14 = QHBoxLayout(self.layoutWidget5)
+        self.horizontalLayout_14.setObjectName(u"horizontalLayout_14")
+        self.horizontalLayout_14.setContentsMargins(0, 0, 0, 0)
+        self.checkBox = QCheckBox(self.layoutWidget5)
+        self.checkBox.setObjectName(u"checkBox")
+
+        self.horizontalLayout_14.addWidget(self.checkBox)
+
+        self.checkBox_2 = QCheckBox(self.layoutWidget5)
+        self.checkBox_2.setObjectName(u"checkBox_2")
+
+        self.horizontalLayout_14.addWidget(self.checkBox_2)
+
+        self.widget = QWidget(self.centralwidget)
+        self.widget.setObjectName(u"widget")
+        self.widget.setGeometry(QRect(20, 40, 235, 32))
+        self.horizontalLayout_2 = QHBoxLayout(self.widget)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.label_3 = QLabel(self.widget)
+        self.label_3.setObjectName(u"label_3")
+
+        self.horizontalLayout_2.addWidget(self.label_3)
+
+        self.comboBox = QComboBox(self.widget)
+        self.comboBox.addItem("")
+        self.comboBox.setObjectName(u"comboBox")
+
+        self.horizontalLayout_2.addWidget(self.comboBox)
+
+        self.pushButton_2 = QPushButton(self.widget)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+
+        self.horizontalLayout_2.addWidget(self.pushButton_2)
+
+        pddApp.setCentralWidget(self.centralwidget)
+        self.statusbar = QStatusBar(pddApp)
+        self.statusbar.setObjectName(u"statusbar")
+        pddApp.setStatusBar(self.statusbar)
+
+        self.retranslateUi(pddApp)
+
+        QMetaObject.connectSlotsByName(pddApp)
+    # setupUi
+
+    def retranslateUi(self, pddApp):
+        pddApp.setWindowTitle(QCoreApplication.translate("pddApp", u"MainWindow", None))
+        self.label_5.setText(QCoreApplication.translate("pddApp", u"\u516c\u5171\u6587\u4ef6\u76ee\u5f55", None))
+        self.toolButton_2.setText(QCoreApplication.translate("pddApp", u"...", None))
+        self.label_6.setText(QCoreApplication.translate("pddApp", u"\u5df2\u4e0a\u4f20\u56fe\u7247\u6587\u4ef6\u914d\u7f6e", None))
+        self.toolButton_3.setText(QCoreApplication.translate("pddApp", u"...", None))
+        self.label_7.setText(QCoreApplication.translate("pddApp", u"\u5546\u54c1\u914d\u7f6e\u8868", None))
+        self.toolButton_4.setText(QCoreApplication.translate("pddApp", u"...", None))
+        self.label_9.setText(QCoreApplication.translate("pddApp", u"sku\u914d\u7f6e\u8868", None))
+        self.toolButton_5.setText(QCoreApplication.translate("pddApp", u"...", None))
+        self.label_10.setText(QCoreApplication.translate("pddApp", u"sku\u914d\u7f6e\u8868SheetName", None))
+        self.label_11.setText(QCoreApplication.translate("pddApp", u"\u5c5e\u6027\u8868SheetName", None))
+        self.label_12.setText(QCoreApplication.translate("pddApp", u"\u578b\u53f7\u5bf9\u7167\u8868", None))
+        self.toolButton_6.setText(QCoreApplication.translate("pddApp", u"...", None))
+        self.label_13.setText(QCoreApplication.translate("pddApp", u"\u578b\u53f7\u5bf9\u7167\u8868SheetName", None))
+        self.checkBox_3.setText(QCoreApplication.translate("pddApp", u"\u662f\u5426\u4e0a\u67b6", None))
+        self.pushButton_3.setText(QCoreApplication.translate("pddApp", u"\u68c0\u6d4b\u56fe\u7247", None))
+        self.pushButton_4.setText(QCoreApplication.translate("pddApp", u"\u68c0\u6d4b\u914d\u7f6e", None))
+        self.pushButton_5.setText(QCoreApplication.translate("pddApp", u"\u5f00\u59cb\u4e0a\u4f20", None))
+        self.label.setText(QCoreApplication.translate("pddApp", u"\u5e97\u94fa\u540d", None))
+        self.label_2.setText(QCoreApplication.translate("pddApp", u"\u5e97\u94faID", None))
+        self.pushButton.setText(QCoreApplication.translate("pddApp", u"\u767b\u51fa", None))
+        self.label_4.setText(QCoreApplication.translate("pddApp", u"\u5957\u56fe\u6587\u4ef6\u76ee\u5f55", None))
+        self.toolButton.setText(QCoreApplication.translate("pddApp", u"...", None))
+        self.label_8.setText(QCoreApplication.translate("pddApp", u"\u5546\u54c1\u8868SheetName", None))
+        self.label_14.setText(QCoreApplication.translate("pddApp", u"sku\u7ec4\u5408\u7c7b\u578b", None))
+        self.comboBox_2.setItemText(0, QCoreApplication.translate("pddApp", u"\u6b3e\u5f0f+\u578b\u53f7", None))
+
+        self.label_15.setText(QCoreApplication.translate("pddApp", u"sku\u6392\u5e8f\u7c7b\u578b", None))
+        self.comboBox_3.setItemText(0, QCoreApplication.translate("pddApp", u"\u578b\u53f7\u5728\u524d", None))
+
+        self.checkBox.setText(QCoreApplication.translate("pddApp", u"\u81ea\u52a8\u751f\u6210sku\u7f16\u7801", None))
+        self.checkBox_2.setText(QCoreApplication.translate("pddApp", u"\u662f\u5426\u63d0\u4ea4", None))
+        self.label_3.setText(QCoreApplication.translate("pddApp", u"\u8fd0\u8d39\u6a21\u677f", None))
+        self.comboBox.setItemText(0, QCoreApplication.translate("pddApp", u"\u767e\u4e16\u6c47\u901a", None))
+
+        self.pushButton_2.setText(QCoreApplication.translate("pddApp", u"\u5237\u65b0", None))
+    # retranslateUi
+
